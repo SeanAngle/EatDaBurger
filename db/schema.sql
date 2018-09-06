086aba1dfbf07ea6e1be459e992638cad952d684
Sean Angle <seanangle@me.com>
2018-09-06 16:23:32 -0500

    Added the information into the database

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,9 @@
+-- Drops the data base if there is already one and makes a new fresh data base
 DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
-
+-- This selects the database we want to use
 USE burgers_db;
-
+-- This makes the able burgers with the columns we need
 CREATE TABLE burgers (
     id INT (11) AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(100) NOT NULL,
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers VALUE("Bacon cheese burger");
+INSERT INTO burgers VALUE("Philly burger");
+INSERT INTO burgers VALUE("Boring cheese burger");
